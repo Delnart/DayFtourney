9bb6230d3a7cd6f1ba555a40039d62e71a90bf6f
delnart <maksimburlaka2008@gmail.com>
2026-04-10 22:34:22 +0300

    fix: correct path to tournament.json in db.js

--- api/src/db.js
+++ api/src/db.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const DATA_FILE = path.join(__dirname, '../../data/tournament.json');
+const DATA_FILE = path.join(__dirname, '../data/tournament.json');
 
 function loadData() {
   return JSON.parse(fs.readFileSync(DATA_FILE, 'utf8'));
